c2edd2ca07a912dad466093be9e752c6dfe19669
Maria Sokolova <mariasokolova@MacBook-Air-Maria.local>
2022-06-14 22:01:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from "./data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [wishes, setWishes] = useState(data);
+
+	const removeWish = (id) => {
+		let newWishes = wishes.filter(wish => wish.id !==id);
+		setWishes(newWishes);
+	}
+
+	return (
+	<div>
+		<div className="container">
+			<h1>My Whishlist for 2022-2023 ({ wishes.length } wishes)</h1>
+		</div>
+		{wishes.map((element=> {
+			const { id, item, image } = element;
+			return(<div key = { id }>
+				<div className="container">
+					<h2> {id} - {item}</h2>
+				</div>
+
+				<div className="container">
+				<img src={ image } width="340" height="300" alt="wish" />
+				</div>
+
+				<div className="container">
+					<button className="custom-btn btn-6" onClick={ ()=> removeWish(id) }> <span>REMOVE</span></button>
+				</div>
+			</div>
+			)
+		}))}
+		<div className="container">
+			<button className="custom-btn btn-6" onClick={()=>setWishes([])}> <span>DELETE ALL</span></button>
+		</div>
+	</div>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const data = [
+{
+	id: 1,
+	item: "Apple AirPods Pro",
+	image:"https://www.bhphotovideo.com/images/images1500x1500/apple_mlwk3am_a_airpods_pro_wireless_headphones_1668453.jpg",
+},
+
+{
+	id: 2,
+	item: "Air balloon ride",
+	image:"https://cf.bstatic.com/data/xphoto/1182x887/382/38250948.jpg?size=S",
+},
+
+{
+	id: 3,
+	item: "LG 75-Inch 4K Smart TV",
+	image:"https://m.media-amazon.com/images/I/A1KGYV6G1kL._AC_SX679_.jpg",
+},
+
+{
+	id: 4,
+	item: "Little birds of my parrot pair",
+	image:"https://moikesha.ru/wp-content/uploads/2016/06/ptency-korelly-foto-1024x768.jpg",
+},
+
+{
+	id: 5,
+	item: "Vacation on Miami beach in fall",
+	image:"https://images.miamiandbeaches.com/getmedia/2ea8352f-9de8-408d-8cdd-83668211dcf1/south-beach-jetty-1440x900.jpg.aspx?width=1440&height=900&ext=.jpg",
+},
+
+{
+	id: 6,
+	item: "Visiting Yellowstone National park",
+	image:"https://www.planetware.com/photos-large/USWY/usa-wyoming-yellowstone-grand-prismatic-spring.jpg",
+},
+{
+	id: 7,
+	item: "Camping trip with family on Delawarу river",
+	image:"https://images.squarespace-cdn.com/content/v1/5cb6391f7980b3404bf57113/1571088778765-330BE0IWR0PQIRQM0KZA/Sunrise+over+the+Delaware+River?format=1000w",
+},
+{
+	id: 8,
+	item: "Echo (3rd Gen) with Alexa",
+	image:"https://m.media-amazon.com/images/I/51EVETDOOeL._AC_SX679_.jpg",
+},
+
+{
+	id: 9,
+	item: "Camera in the car",
+	image:"https://m.media-amazon.com/images/I/716v5Bo4+sL._AC_SX522_.jpg",
+},
+
+{
+	id: 10,
+	item: "Patio furniture set with fire pit",
+	image:"https://m.media-amazon.com/images/I/81NIKCVZOuL._AC_SX679_.jpg",
+},
+]
